04bbd5e12047b942feb5f97318f0bf703b61feb9
Nahdhiana Hasmi <14351410+nahdhiana@users.noreply.github.com>
2021-11-06 17:43:39 +0700

    cleanup

--- raspbianOnScreenKeyboardInstall.sh
+++ raspbianOnScreenKeyboardInstall.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y matchbox-keyboard
-sudo apt install -y clean
-sudo apt install -y autoclean
-sudo apt install autoremove -y
+sudo apt install matchbox-keyboard -y
 sudo reboot
